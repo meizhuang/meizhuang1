fc99df251239026697b3043888b4a08f7292116e
WangTaoKnight <wangtao199205@qq.com>
2015-06-30 23:01:37 +0800

    12222

--- 11/11/ViewController.m
+++ 11/11/ViewController.m
@@ -4,11 +4,8 @@
 //
 //  Created by 高才新 on 15/6/30.
 //  Copyright (c) 2015年 高才新. All rights reserved.
-<<<<<<< HEAD
 //11
-=======
 // rrrrrrrrr
->>>>>>> origin/master
 
 #import "ViewController.h"
 
